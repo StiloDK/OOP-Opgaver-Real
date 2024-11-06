5dcae96aff24105e4ac3a050bc0433c5fa30e47d
StiloDK <86182820+StiloDK@users.noreply.github.com>
2024-11-06 10:18:12 +0100

    Arv Updates to Opgave (With Marius)

---
+++ Account.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indkapsling_C_
+{
+    internal abstract class User
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        protected User(string firstName, string lastName)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
+        public string FullName => $"{FirstName} {LastName}";
+    }
+
+    internal class Owner : User
+    {
+        public int KundeID { get; }
+
+        public Owner(int kundeID, string firstName, string lastName)
+            : base(firstName, lastName)
+        {
+            KundeID = kundeID;
+        }
+    }
+
+    internal class Admin : User
+    {
+        public double AdminBalance { get; private set; }
+        public int AdminID { get; }
+
+        public Admin(double initialBalance, int adminID, string firstName, string lastName)
+            : base(firstName, lastName)
+        {
+            AdminBalance = initialBalance;
+            AdminID = adminID;
+        }
+
+        public Account CreateAccount(Owner owner)
+        {
+            var account = new Account(AdminBalance, owner.KundeID, owner);
+            Console.WriteLine($"Hej {owner.FullName}. Din konto er oprettet med {FullName} som admin.");
+            return account;
+        }
+    }
+
+    internal class Account
+    {
+        public double Balance { get; private set; }
+        public int ID { get; }
+        public Owner AccountOwner { get; }
+
+        public Account(double initialBalance, int id, Owner owner)
+        {
+            Balance = initialBalance;
+            ID = id;
+            AccountOwner = owner;
+        }
+
+        public string AddBalance(double amount)
+        {
+            Balance += amount;
+            return UpdateMessage();
+        }
+
+        public string RemoveBalance(double amount)
+        {
+            Balance -= amount;
+            return UpdateMessage();
+        }
+
+        private string UpdateMessage()
+        {
+            return $"Din konto er blevet opdateret. Der står nu kr. {Balance}.";
+        }
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using Indkapsling_C_;
+
+namespace Indkapsling_CSharp
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Owner owner = new Owner(123456, "Daniel", "Jensen");
+
+            Account account = new Account(100, 123456, owner);
+            Console.WriteLine($"Hej {owner.FullName}, din konto er oprettet.");
+
+            double depositAmount = Convert.ToDouble(Console.ReadLine());
+            string depositMessage = account.AddBalance(depositAmount);
+            Console.WriteLine(depositMessage);
+
+            double withdrawAmount = Convert.ToDouble(Console.ReadLine());
+            string withdrawMessage = account.RemoveBalance(withdrawAmount);
+            Console.WriteLine(withdrawMessage);
+
+        }
+    }
+}
