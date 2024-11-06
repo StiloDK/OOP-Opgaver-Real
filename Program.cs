ece832cf359cdc4d2bada5d42389e6a90d009b19
StiloDK <86182820+StiloDK@users.noreply.github.com>
2024-11-06 11:54:29 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.ConstrainedExecution;
+
+namespace VehicleInspection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car
+            {
+                Brand = "Audi",
+                Model = "A4",
+                ProductionDate = new DateTime(2018, 7, 1),
+                LastInspection = new DateTime(2022, 12, 8)
+            };
+
+            Truck truck = new Truck
+            {
+                Brand = "Volvo",
+                Model = "FH",
+                ProductionDate = new DateTime(2018, 10, 1),
+                LastInspection = new DateTime(2021, 10, 1)
+            };
+
+            Console.WriteLine(car.DisplyInfo() + (car.InspectionStatus() ? " - Requires Inspection" : " - No Inspection Needed"));
+            Console.WriteLine(truck.DisplyInfo() + (truck.InspectionStatus() ? " - Requires Inspection" : " - No Inspection Needed"));
+        }
+    }
+}
---
+++ VehicleInspection.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VehicleInspection
+{
+    public abstract class Vehicle
+    {
+        public string? Brand { get; set; }
+        public string? Model { get; set; }
+        public DateTime ProductionDate { get; set; }
+        public DateTime LastInspection { get; set; }
+
+        public abstract bool InspectionStatus();
+
+        public virtual string DisplyInfo()
+        {
+            return $"Vehicle: {Brand} {Model}, Production Date: {ProductionDate.ToShortDateString()}";
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public override bool InspectionStatus()
+        {
+            int yearsSinceProduction = DateTime.Now.Year - ProductionDate.Year;
+            int yearsSinceLastInspection = DateTime.Now.Year - LastInspection.Year;
+
+            return yearsSinceProduction > 4 && yearsSinceLastInspection > 2;
+        }
+
+        public override string DisplyInfo()
+        {
+            return $"Car: {Brand} {Model}";
+        }
+    }
+
+    public class Truck : Vehicle
+    {
+        public override bool InspectionStatus()
+        {
+            int yearsSinceProduction = DateTime.Now.Year - ProductionDate.Year;
+            int yearsSinceLastInspection = DateTime.Now.Year - LastInspection.Year;
+
+            return yearsSinceProduction > 1 && yearsSinceLastInspection > 1;
+        }
+
+        public override string DisplyInfo()
+        {
+            return $"Yruck: {Brand} {Model}";
+        }
+    }
+}
